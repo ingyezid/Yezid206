7121727430a7393ff9dd90580a97938bd60d30ac
estudiante <mail@gmail.com>
2018-10-03 12:20:53 -0500

    Archivos de Ruby

---
+++ practica1.rb
@@ -0,0 +1,12 @@
+
+puts "Ingrese un numero"
+numero =  gets.chomp.to_i
+
+if numero % 2 == 0
+  puts " El numero #{numero} es par"
+else if numero % 2 !=0
+    puts "El numero #{numero} es impar"
+else
+  puts "No se cumplio ninguna condicion"
+  end
+end
---
+++ practica2.rb
@@ -0,0 +1,12 @@
+puts "Ingrese un nombre"
+name = gets.chomp.to_s
+
+case name
+when "Jose"
+  puts  "#{name} es padre de Jesus"
+when "Maria"
+  puts "#{name} es la madre de Jesus"
+else 
+  puts "No se cumplio ninguna condicion"
+
+end
